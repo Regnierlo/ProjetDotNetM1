d2d55b66623fb0ab2ef39ae30b710ed7d8434511
hubos89 <fromentin.hugo@gmail.com>
2018-01-27 20:11:44 +0100

    Comit on test

--- ProjetDotNetM1/GestionListeImages.cs
+++ ProjetDotNetM1/GestionListeImages.cs
@@ -8,39 +8,52 @@ namespace ProjetDotNetM1
 {
     class GestionListeImages
     {
-        ArrayList listeImg;
+        public ArrayList ListeImg
+        {
+            get;
+            set;
+        }
         /*
          * creer une liste de gestionImage a partir du dossier d'importation
-         */ 
+         */
         public GestionListeImages(ProgressBar bar, DataGridView grid)
         {
             bar.Visible = true;
-            listeImg = new ArrayList();
+            ListeImg = new ArrayList();
             string url = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
             url = Path.Combine(url, "FHRImages");
-            System.Collections.IEnumerable files = Directory.EnumerateFiles(url);
-            int nb = 0;
-            foreach (string img in files)
+            try
             {
-                nb++;
-            }
-            bar.Maximum = nb-1;
-            bar.Minimum = 0;
-            bar.Step = 1;
-            nb = 0;
-            foreach (string img in files)
-            {
-                Char delim = '\\';
-                string[] name = img.Split(delim);
-                if(name[name.Count()-1] != "Thumbs.db")
+                System.Collections.IEnumerable files = Directory.EnumerateFiles(url);
+                int nb = 0;
+                foreach (string img in files)
                 {
-                    listeImg.Add(new GestionImage(img));
-                    bar.PerformStep();
+                    nb++;
                 }
+                bar.Maximum = nb-1;
+                bar.Minimum = 0;
+                bar.Step = 1;
+                nb = 0;
+                foreach (string img in files)
+                {
+                    Char delim = '\\';
+                    string[] name = img.Split(delim);
+                    if(name[name.Count()-1] != "Thumbs.db")
+                    {
+                        ListeImg.Add(new GestionImage(img));
+                        bar.PerformStep();
+                    }
                 
+                }
+                bar.Value = 0;
+                bar.Visible = false;
             }
-            bar.Value = 0;
-            bar.Visible = false;
+            catch(Exception)
+            {
+                System.IO.Directory.CreateDirectory(url);
+                bar.Visible = false;
+            }
+            
         }
 
         /*
@@ -48,9 +61,9 @@ namespace ProjetDotNetM1
          */ 
         public GestionListeImages(ArrayList entryList)
         {
-            listeImg = new ArrayList();
+            ListeImg = new ArrayList();
             foreach ( string img in entryList ) {
-                listeImg.Add(new GestionImage(img));
+                ListeImg.Add(new GestionImage(img));
             }
         }
 
@@ -59,7 +72,7 @@ namespace ProjetDotNetM1
          */ 
         public void importer()
         {
-            foreach (GestionImage img in listeImg)
+            foreach (GestionImage img in ListeImg)
             {
                 Char delim = '\\';
                 string url = img.ImgUrl;
@@ -72,10 +85,30 @@ namespace ProjetDotNetM1
                 {
                     System.IO.File.Copy(img.ImgUrl, saveUrl, false);
                 }
-                catch (Exception ex)
+                catch (System.IO.DirectoryNotFoundException)
                 {
                     System.IO.Directory.CreateDirectory(saveUrlDos);
-                    System.IO.File.Copy(img.ImgUrl, saveUrl, false);    //possible exception si le fkchier deja present a voir comment on le gere ;)
+                    System.IO.File.Copy(img.ImgUrl, saveUrl, false);
+                }
+                catch (System.IO.IOException)
+                {
+                    FormImageExistante form = new FormImageExistante(img.Img,saveUrl);
+                    DialogResult res = form.ShowDialog();
+                    if(res == DialogResult.Cancel)
+                    {
+                        System.Console.WriteLine("cancel : 1");
+                    }
+                    else
+                    {
+                        if(res == DialogResult.Ignore)
+                        {
+                            System.Console.WriteLine("ignore : 3");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("abort : 2");
+                        }
+                    }
                 }
             }
         }
