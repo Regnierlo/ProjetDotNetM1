a05b534df68fa7c3d99f9ff5b89f1a9c5b354810
regnierlo <lois.regnier.93@gmail.com>
2018-01-29 12:26:31 +0100

    Début de la gestion du fichier XML pour les tags

---
+++ ProjetDotNetM1/GestionXML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace ProjetDotNetM1
+{
+    public class GestionXML
+    {
+        #region Variables de classe
+        private static GestionXML _instance;
+        private String _chemin;
+        private XmlDocument _doc;
+        #endregion
+
+        #region Proprietes
+        public String Chemin
+        {
+            get { return _chemin; }
+            set { _chemin = value; }
+        }
+        #endregion
+
+        public GestionXML()
+        {
+            _chemin = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            _chemin = Path.Combine(_chemin, "FHRImages");
+            _doc = new XmlDocument();
+        }
+
+        //Singleton
+        public static GestionXML Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new GestionXML();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Ajouter un noeud sans pere.
+        /// </summary>
+        public void AjouterNoeud()
+        {
+            
+        }
+    }
+}
